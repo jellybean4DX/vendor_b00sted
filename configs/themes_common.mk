79cf790f359c6fbad4d0903a73e16d6100266b86
BMc08GT <bmc08gt.teamb00sted@gmail.com>
2012-05-16 18:12:07 -0400

    Androidian Theme

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -2,8 +2,10 @@
 PRODUCT_PACKAGES += \
        ThemeManager \
        ThemeChooser \
-       com.tmobile.themes
+       com.tmobile.themes \
+       Androidian
 
 # ThemeChooser Permissions
 PRODUCT_COPY_FILES += \
        vendor/b00sted/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+
