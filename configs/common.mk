4e4bdd67e71d180b196abd9c305930e8f74f387e
BMc08GT <bmc08gt.teamb00sted@gmail.com>
2012-05-18 09:46:26 -0400

    Typo and add sysinit and userinit init.d script

--- configs/common.mk
+++ configs/common.mk
@@ -69,9 +69,11 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PREBUILT)/app/Superuser.apk:system/app/Superuser.apk \
     $(COMMON_PREBUILT)/app/LatinImeDictionaryPack.apk:system/app/LatinImeDictionaryPack.apk \
     $(COMMON_PREBUILT)/app/ApexLauncher_v1.1.1.apk:system/app/ApexLauncher_v1.1.1.apk \
-    $(COMMON_PREBUILT)/bin/su:system/bin.su \
+    $(COMMON_PREBUILT)/bin/su:system/bin/su \
+    $(COMMON_PREBUILT)/bin/sysinit:system/bin/sysinit \
     $(COMMON_PREBUILT)/etc/resolv.conf:system/etc/resolv.conf \
     $(COMMON_PREBUILT)/etc/sysctl.conf:system/etc/sysctl.conf \
+    $(COMMON_PREBUILT)/etc/init.d/90userinit:system/etc/init.d/90userinit \
     $(COMMON_PREBUILT)/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
     $(COMMON_PREBUILT)/media/bootanimation.zip:system/media/bootanimation.zip
 
