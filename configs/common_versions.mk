0903f4e7f2a9db0873087c260ef1b8e9ad9ac274
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-06-08 09:18:42 -0400

    Update buildID to 4.0.4_r2.1 (IMM76L)

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,6 +1,6 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += \
-BUILD_DISPLAY_ID=IMM76I \
+BUILD_DISPLAY_ID=IMM76L \
 BUILD_VERSION_TAGS=release-keys \
 USER=teamb00sted \
 BUILD_UTC_DATE=$(shell date +"%s")
