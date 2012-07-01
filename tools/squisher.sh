2e434e6b2a69ecd921e420ae90a19b73bef4a4cc
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-07-01 04:03:58 -0300

    allow able to work with all OUT dirs

--- tools/squisher.sh
+++ tools/squisher.sh
@@ -1,7 +1,7 @@
+#!/bin/sh
 # This script is included in squisher
 # It is the final build step (after OTA package)
 
-DEVICE_OUT=$ANDROID_BUILD_TOP/out/target/product/shadow
 DEVICE_TOP=$ANDROID_BUILD_TOP/device/motorola/omap34com
 
 
@@ -16,21 +16,21 @@ mkdir -p $REPACK/ota/system/bootmenu
 	cp -rf $DEVICE_TOP/bootmenu/* $REPACK/ota/system/bootmenu/
 
 mkdir -p $REPACK/ota/system/bootmenu/2nd-init
-	cp -f $DEVICE_OUT/root/init $REPACK/ota/system/bootmenu/2nd-init/init
+	cp -f $OUT/root/init $REPACK/ota/system/bootmenu/2nd-init/init
 	cp -f $DEVICE_TOP/*.rc $REPACK/ota/system/bootmenu/2nd-init/
 	cp -f $DEVICE_TOP/bootmenu/config/default.prop $REPACK/ota/system/bootmenu/2nd-init
 
 mkdir -p $REPACK/ota/system/bootmenu/2nd-init/sbin
-	cp -f $DEVICE_OUT/symbols/hijack-boot/2nd-init $REPACK/ota/system/bootmenu/2nd-init/sbin/2nd-init
-	cp -f $DEVICE_OUT/symbols/hijack-boot/getprop $REPACK/ota/system/bootmenu/2nd-init/sbin/getprop
-	cp -f $DEVICE_OUT/symbols/hijack-boot/stop $REPACK/ota/system/bootmenu/2nd-init/sbin/stop
-	cp -f $DEVICE_OUT/symbols/sbin/adbd $REPACK/ota/system/bootmenu/2nd-init/sbin/adbd
+	cp -f $OUT/symbols/hijack-boot/2nd-init $REPACK/ota/system/bootmenu/2nd-init/sbin/2nd-init
+	cp -f $OUT/symbols/hijack-boot/getprop $REPACK/ota/system/bootmenu/2nd-init/sbin/getprop
+	cp -f $OUT/symbols/hijack-boot/stop $REPACK/ota/system/bootmenu/2nd-init/sbin/stop
+	cp -f $OUT/symbols/sbin/adbd $REPACK/ota/system/bootmenu/2nd-init/sbin/adbd
 
 mkdir -p $REPACK/ota/system/bootmenu/binary
-	cp -f $DEVICE_OUT/symbols/hijack-boot/2nd-init $REPACK/ota/system/bootmenu/binary/2nd-init
-	cp -f $DEVICE_OUT/system/xbin/busybox $REPACK/ota/system/bootmenu/binary/busybox
-	cp -f $DEVICE_OUT/symbols/utilities/lsof $REPACK/ota/system/bootmenu/binary/lsof
-	cp -f $DEVICE_OUT/symbols/sbin/adbd $REPACK/ota/system/bootmenu/binary/adbd
+	cp -f $OUT/symbols/hijack-boot/2nd-init $REPACK/ota/system/bootmenu/binary/2nd-init
+	cp -f $OUT/system/xbin/busybox $REPACK/ota/system/bootmenu/binary/busybox
+	cp -f $OUT/symbols/utilities/lsof $REPACK/ota/system/bootmenu/binary/lsof
+	cp -f $OUT/symbols/sbin/adbd $REPACK/ota/system/bootmenu/binary/adbd
 	mv $REPACK/ota/system/bin/logwrapper $REPACK/ota/system/bootmenu/binary/logwrapper.bin
 
 cp -rf $ANDROID_BUILD_TOP/external/bootmenu/images $REPACK/ota/system/bootmenu/images
@@ -41,12 +41,12 @@ mkdir -p $REPACK/ota/system/bootmenu/ext/modules
 	cp -f $DEVICE_TOP/modules/overclock.ko $REPACK/ota/system/bootmenu/ext/modules
 
 mkdir -p $REPACK/ota/system/bootmenu/recovery/sbin
-	cp -rfd $DEVICE_OUT/recovery/root/sbin/* $REPACK/ota/system/bootmenu/recovery/sbin
+	cp -rfd $OUT/recovery/root/sbin/* $REPACK/ota/system/bootmenu/recovery/sbin
 	find $REPACK/ota/system/bootmenu/recovery/sbin/ -type l -exec rm {} \; # remove symbolic links
 mkdir -p $REPACK/ota/system/bootmenu/recovery/res
-	cp -rf $DEVICE_OUT/recovery/root/res/* $REPACK/ota/system/bootmenu/recovery/res 
+	cp -rf $OUT/recovery/root/res/* $REPACK/ota/system/bootmenu/recovery/res 
 mkdir -p $REPACK/ota/system/bootmenu/recovery/etc
-	cp -rf $DEVICE_OUT/recovery/root/etc/* $REPACK/ota/system/bootmenu/recovery/etc
-	cp -f $DEVICE_OUT/recovery/root/etc/recovery.fstab $REPACK/ota/system/bootmenu/recovery/recovery.fstab
-	cp -f $DEVICE_OUT/recovery/root/etc/recovery.fstab $REPACK/ota/system/bootmenu/recovery/etc/recovery.fstab
+	cp -rf $OUT/recovery/root/etc/* $REPACK/ota/system/bootmenu/recovery/etc
+	cp -f $OUT/recovery/root/etc/recovery.fstab $REPACK/ota/system/bootmenu/recovery/recovery.fstab
+	cp -f $OUT/recovery/root/etc/recovery.fstab $REPACK/ota/system/bootmenu/recovery/etc/recovery.fstab
 
