469093593dbc14469d151d3644e945a8b8c41529
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-07-03 06:46:45 -0300

    Add note re: hacked naming method

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,8 @@
+
+# Use our hack method to name the zips correctly
+# pr0ject-0$(TARGET_PRODUCT)-shell date.zip
+# leaves us with project-0map_shadow-shell date.zip
+
 add_lunch_combo map_shadow-userdebug
 #add_lunch_combo b00stedICS_droid2-userdebug
 #add_lunch_combo b00stedICS_droid2we-userdebug
